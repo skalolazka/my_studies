993ac9b844e2deb07f33fbd3bbb8545263c67317
Strelkova Natalia <skalolazka@yandex-team.ru>
2014-06-03 20:53:36 +0400

    6.1.py (please don't make me delete the comments, I really don't think it matters)

---
+++ 6.1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys
+# takes array and i, rearranges array - first elements < array[i],
+# then equal, then greater
+
+def rearrange (arr, i):
+    """
+    >>> rearrange([0], 0)
+    [0]
+    >>> rearrange([0, 1], 0)
+    [0, 1]
+    >>> rearrange([0, 1], 1)
+    [0, 1]
+    >>> rearrange([0, 1, 0], 0)
+    [0, 0, 1]
+    >>>
+    >>> rearrange([0, 1, 2, 0, 2, 1, 0], 1)
+    [0, 0, 0, 1, 1, 2, 2]
+    >>> rearrange([2, 2, 0, 0], 1)
+    [0, 0, 2, 2]
+    """
+    pivot = arr[i]
+    middle_index = 0
+    big_index = 0
+    for j in range(len(arr)):
+#        print 'start '+str(locals())
+        if arr[j] < pivot:
+#            print '<'
+            (arr[j], arr[middle_index]) = (arr[middle_index], arr[j])
+            (arr[j], arr[big_index]) = (arr[big_index], arr[j])
+            middle_index += 1
+            big_index += 1
+        elif arr[j] == pivot:
+#            print '=='
+            (arr[j], arr[big_index]) = (arr[big_index], arr[j])
+            big_index += 1
+#        else: # arr[j] > pivot
+#            print '>'
+#            (arr[j], arr[big_index]) = (arr[big_index], arr[j])
+#        print 'end '+str(locals())
+    return arr
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose = False, optionflags= doctest.REPORT_ONLY_FIRST_FAILURE)
+#    print rearrange([0, 2, 1, 0, 1, 2, 3], 2)

2c358ed141719b8773d969dffe020eea33594424
Strelkova Natalia <skalolazka@yandex-team.ru>
2014-06-06 12:36:20 +0400

    encode-decode: error in encode fixed (empty string)

--- 6.18.pl
+++ 6.18.pl
@@ -15,7 +15,7 @@ sub encode {
     my ($prev, $count, $encoded) = ($str[0], 1, '');
 
     for (my $j = 1; $j < scalar(@str); $j++) {
-        warn "j $j str $str[$j]";
+#        warn "j $j str $str[$j]";
         if ($str[$j] ne $prev) {
             $encoded .= "$count$prev";
             $count = 0;
@@ -23,7 +23,7 @@ sub encode {
         $count++;
         $prev = $str[$j];
     }
-    $encoded .= "$count$prev";
+    $encoded .= $prev ? "$count$prev" : '';
     return $encoded;
 }
 
@@ -45,12 +45,15 @@ use Test::More;
 use Test::Deep;
 
 my $module = MyEncoding->new;
+is($module->encode(''), '', 'empty str OK');
 is($module->encode('a'), '1a', 'a OK');
 is($module->encode('aaa'), '3a', 'aaa OK');
 is($module->encode('aacccb'), '2a3c1b', 'aaccb OK');
 is($module->encode('aaaacbbb'), '4a1c3b', 'aaaacbbb OK');
+is($module->decode(''), '', 'decode: empty str OK');
 is($module->decode('1a'), 'a', 'decode: 1a OK');
 is($module->decode('2a'), 'aa', 'decode: 2a OK');
 is($module->decode('3a1c2b6d'), 'aaacbbdddddd', 'decode: 3a1c2b6d OK');
+is($module->decode('1a2b1a'), 'abba', 'decode: 1a2b1a OK');
 
 done_testing;

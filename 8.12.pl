f4c1a654c7d0f43aa185347c21799906a14c77fa
Strelkova Natalia <flore@ya.ru>
2014-07-03 09:03:52 +0400

    8.12. - not tested!!!

---
+++ 8.12.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package MyQueueFromStacks;
+
+sub new {
+    my ($class, @values) = @_;
+    my $self = {
+        full_stack => \@values,
+        empty_stack => [],
+    };
+    # let us use just arrays for stacks here, I don't want
+    # to rewrite the stack class again
+    return bless $self => $class;
+}
+
+sub enqueue {
+    my ($self, $val) = @_;
+    push(@{$self->{full_stack}}, $val);
+    return $val;
+}
+
+sub dequeue {
+    my $self = shift;
+    return undef unless scalar(@{$self->{full_stack}});
+    my $val = pop(@{$self->{full_stack}});
+    while (defined($val)) {
+        push(@{$self->{empty_stack}}, $val);
+        $val = pop(@{$self->{full_stack}});
+    }
+    my $result = pop(@{$self->{empty_stack}});
+    $val = pop(@{$self->{empty_stack}});
+    while (defined($val)) {
+        push(@{$self->{full_stack}}, $val);
+        $val = pop(@{$self->{empty_stack}});
+    }
+    return $result;
+}
+
+
+package TestMyQueueFromStacks;
+
+my $m = MyQueueFromStacks->new;
+
+use Test::More;
+
+use Data::Dumper;
+print Dumper($m);
+is($m->dequeue, undef, 'nothing to dequeue');
+is($m->enqueue(2), 2, 'enqueued');
+$m->enqueue(3);
+$m->enqueue(4);
+$m->enqueue(5);
+is($m->dequeue, 2, 'one');
+is($m->dequeue, 3, 'two');
+is($m->dequeue, 4, 'three');
+is($m->dequeue, 5, 'four');
+is($m->dequeue, undef, 'none left');
+
+done_testing();

b048517df2280588770f4dfe478e34d109837720
Strelkova Natalia <flore@ya.ru>
2014-07-11 21:46:43 +0400

    fizzbuzz

--- fizz_buzz.pl
+++ fizz_buzz.pl
@@ -4,9 +4,10 @@ use strict;
 
 sub fizz_buzz {
     for (my $i = 1; $i <= 100; $i++) {
-        if ($i % 3 == 0) { print "fizz\n" }
-        if ($i % 5 == 0) { print "buzz\n" }
-        if ($i % 3 && $i % 5) { print "$i\n" }
+        if ($i % 3 == 0) { print "fizz" }
+        if ($i % 5 == 0) { print "buzz" }
+        if ($i % 3 && $i % 5) { print $i }
+        print "\n"
     }
 }
 

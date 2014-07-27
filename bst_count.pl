20963f18c753a2f803f900be6e490dc0bbdba16e
Strelkova Natalia <flore@ya.ru>
2014-07-27 11:41:53 +0400

    bst_count - function to count the number of different bst's by the number of nodes

---
+++ bst_count.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+my @data = (1, 1);
+
+sub bst_count {
+    my ($level) = @_;
+    return $data[$level] if ($data[$level]);
+    my $count = 0;
+    for (my $i = $level - 1; $i >= 0; $i--) {
+        $count += bst_count($i) * bst_count($level - $i - 1);
+    }
+    $data[$level] = $count;
+    return $count;
+}
+
+use Test::More;
+
+is(bst_count(0), 1, 'zero');
+is(bst_count(1), 1, 'one');
+is(bst_count(2), 2, 'two');
+is(bst_count(3), 5, 'three');
+is(bst_count(4), 14, 'four');
+is(bst_count(5), 42, 'five');
+
+done_testing;

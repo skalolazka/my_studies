b1981cc7c435e8b5be8c452eef64b7edac7f6744
Strelkova Natalia <flore@ya.ru>
2014-08-13 22:48:42 +0400

    10.8. (running median) - fixes

--- 10.8.py
+++ 10.8.py
@@ -9,39 +9,35 @@ class MyNodeWithMid(MyNode):
         MyNode.__init__(self, value)
         self.mid1 = None
         self.mid2 = None
+
+
+class MyNodesWithMid(MyNodeWithMid):
+    def __init__(self, value=None):
+        self.start = MyNodeWithMid(value)
+        self.start.mid1 = self.start
+
     def median(self):
-        if self.mid1 is not None:
-            if self.mid2 is not None: # is even
-                return (float(self.mid1.value) + float(self.mid2.value)) / 2
-            else: # is odd
-                return self.mid1.value
-        else: # start
-            return self.value
+        if self.start.mid2 is not None: # is even
+            return (float(self.start.mid1.value) + float(self.start.mid2.value)) / 2
+        else: # is odd
+            return self.start.mid1.value
 
-def add_head(node, value=None):
-    new_node = MyNodeWithMid(value)
-    if node.mid1 is not None:
-        if node.mid2 is not None: # was even
-            new_node.mid1 = node.mid2
-        else: # was odd
-            new_node.mid1 = node.mid1
-            new_node.mid2 = node.mid1.next_node
-    else: # start
-        new_node.mid1 = node
-        new_node.mid2 = new_node
-    node.mid1, node.mid2 = None, None
-    node.next_node = new_node
-    return new_node
+    def add_number(self, value=None):
+        old = self.start
+        self.start = MyNodeWithMid(value)
+        old.next_node = self.start
+        if old.mid2 is None:
+            self.start.mid1 = old.mid1.next_node
+            self.start.mid2 = old.mid1
+        else:
+            self.start.mid1 = old.mid1
+        old.mid1, old.mid2 = None, None
 
-
-def running_median(node, val):
-    node.add_node(val)
-    return node.median()
 
 import unittest
 
 class MyTest(unittest.TestCase):
-    def test_class(self):
+    def test_base_class(self):
         n = MyNodeWithMid(1)
         self.assertIsInstance(n, MyNodeWithMid, 'type')
         self.assertEqual(n.value, 1, 'value')
@@ -49,38 +45,65 @@ class MyTest(unittest.TestCase):
         self.assertEqual(n.mid1, None, 'mid1')
         self.assertEqual(n.mid2, None, 'mid2')
 
+    def test_class(self):
+        n = MyNodesWithMid(1)
+        self.assertIsInstance(n, MyNodeWithMid, 'type base')
+        self.assertIsInstance(n, MyNodesWithMid, 'type')
+        self.assertEqual(n.start.value, 1, 'value')
+        self.assertEqual(n.start.next_node, None, 'next')
+        self.assertEqual(n.start.mid1, n.start, 'mid1')
+        self.assertEqual(n.start.mid2, None, 'mid2')
+
     def test_add(self):
-        n = MyNodeWithMid(1)
-        n = add_head(n, 2)
+        n = MyNodesWithMid(1)
+        n.add_number(2)
         self.assertIsInstance(n, MyNodeWithMid, 'type')
-        self.assertEqual(n.value, 2, 'value')
-        self.assertEqual(n.mid1.value, 1, 'mid1')
-        self.assertEqual(n.mid2.value, 2, 'mid2')
-        n = add_head(n, 3)
-        self.assertEqual(n.value, 3, 'value')
-        self.assertEqual(n.mid1.value, 2, 'mid1')
-        self.assertEqual(n.mid2, None, 'mid2')
-        n = add_head(n, 4)
-        self.assertEqual(n.value, 4, 'value')
-        self.assertEqual(n.mid1.value, 2, 'mid1')
-        self.assertEqual(n.mid2.value, 3, 'mid2')
-        n = add_head(n, 5)
-        self.assertEqual(n.value, 5, 'value')
-        self.assertEqual(n.mid1.value, 3, 'mid1')
-        self.assertEqual(n.mid2, None, 'mid2')
+        self.assertEqual(n.start.value, 2, 'value')
+        self.assertEqual(n.start.mid1.value, 2, 'mid1')
+        self.assertEqual(n.start.mid2.value, 1, 'mid2')
+        n.add_number(3)
+        self.assertEqual(n.start.value, 3, 'value')
+        self.assertEqual(n.start.mid1.value, 2, 'mid1')
+        self.assertEqual(n.start.mid2, None, 'mid2')
+        n.add_number(4)
+        self.assertEqual(n.start.value, 4, 'value')
+        self.assertEqual(n.start.mid1.value, 3, 'mid1')
+        self.assertEqual(n.start.mid2.value, 2, 'mid2')
+        n.add_number(5)
+        self.assertEqual(n.start.value, 5, 'value')
+        self.assertEqual(n.start.mid1.value, 3, 'mid1')
+        self.assertEqual(n.start.mid2, None, 'mid2')
+        n.add_number(6)
+        self.assertEqual(n.start.value, 6, 'value')
+        self.assertEqual(n.start.mid1.value, 4, 'mid1')
+        self.assertEqual(n.start.mid2.value, 3, 'mid2')
+        n.add_number(0)
+        self.assertEqual(n.start.value, 0, 'value')
+        self.assertEqual(n.start.mid1.value, 4, 'mid1')
+        self.assertEqual(n.start.mid2, None, 'mid2')
 
     def test_median(self):
-        n = MyNodeWithMid(1)
-        n = add_head(n, 2)
+        n = MyNodesWithMid(1)
+        n.add_number(2)
         self.assertEqual(n.median(), 1.5, '2')
-        n = add_head(n, 3)
+        n.add_number(3)
         self.assertEqual(n.median(), 2, '3')
-        n = add_head(n, 4)
+        n.add_number(4)
         self.assertEqual(n.median(), 2.5, '4')
-        n = add_head(n, 5)
+        n.add_number(5)
         self.assertEqual(n.median(), 3, '5')
-        n = add_head(n, 6)
+        n.add_number(6)
         self.assertEqual(n.median(), 3.5, '6')
+        n.add_number(0)
+        self.assertEqual(n.median(), 4, '7 = 0')
+
+    def test_median2(self):
+        n = MyNodesWithMid(1)
+        n.add_number(1)
+        n.add_number(2)
+        n.add_number(0)
+        n.add_number(0)
+        self.assertEqual(n.median(), 2, '2')
 
 
 if __name__ == '__main__':

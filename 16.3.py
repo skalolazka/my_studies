86efc756a872877b4810feb6f500b9226f56034c
Strelkova Natalia <flore@ya.ru>
2014-08-15 11:54:27 +0400

    16.3 - pins and wire

---
+++ 16.3.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# task: pins and wire, can we divide pins into left & right, wires only from left to right
+
+from mygraph import MyGraph
+
+def pins(data):
+    g = MyGraph()
+    for d in data:
+        g.add_edge(d[0], d[1])
+    return paint(g, {'white': [], 'black': []}, g.data[0])[0]
+
+def paint(g, painted, start_vertex=None):
+    if start_vertex is not None: # start with something
+        painted['white'].append(start_vertex[0])
+        for dd in start_vertex[1]:
+            painted['black'].append(dd)
+        for dd in start_vertex[1]:
+            (res, painted) = paint(g, painted)
+            if res == 0:
+                return (res, painted)
+    else:
+        for d in g.data:
+            color = None
+            if d[0] in painted['white']:
+                color = 'black'
+                opposite = 'white'
+            if d[0] in painted['black']:
+                color = 'white'
+                opposite = 'black'
+            if color is not None: # have painted this one - paint children
+                new = 0
+                for dd in d[1]:
+                    if dd in painted[opposite]:
+                        return (0, painted)
+                    if dd not in painted[color]:
+                        new = 1
+                        painted[color].append(dd)
+                if new != 0: # have at least something to paint here
+                    for dd in d[1]:
+                        res = paint(g, painted)
+                        if res == 0:
+                            return (res, painted)
+    for d in g.data: # check if there are not painted vertices yet
+    # means the graph has more than one connected component
+        if d[0] not in painted['white'] and d[0] not in painted['black']:
+            paint(g, painted, d)
+    return (1, painted)
+
+
+import unittest
+
+class TestPins(unittest.TestCase):
+    def test_small(self):
+        self.assertEqual(pins([[1,2],[1,3]]), 1, 'ok')
+        self.assertEqual(pins([[1,2],[2,3]]), 1, 'ok')
+        self.assertEqual(pins([[1,2],[2,3],[1,3]]), 0, 'not ok')
+
+    def test_big(self):
+        self.assertEqual(pins([[1,2],[1,4],[1,8],[2,3],[3,4],[4,5],[5,6],[6,7],[7,8],[7,2]]), 1, 'big ok')
+
+    def test_big_no(self):
+        self.assertEqual(pins([[1,2],[1,4],[1,8],[4,5],[5,7],[7,8],[7,2]]), 0, 'big ok')
+
+    def test_two_components(self):
+        self.assertEqual(pins([[1,2],[2,3],[3,4],[1,3],[5,6],[6,7],[5,7]]), 0, 'two components not ok')
+        self.assertEqual(pins([[1,2],[2,3],[3,4],[1,4],[5,6],[6,7],[7,8],[8,9],[5,8]]), 1, 'two components ok')
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPins)
+    unittest.TextTestRunner(verbosity=2).run(suite)

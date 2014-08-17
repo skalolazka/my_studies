9c60ebd444ef38c7eabcec2e9970f97c42ce65ab
Strelkova Natalia <flore@ya.ru>
2014-08-17 15:03:04 +0400

    mygraph: one more test

--- mygraph.py
+++ mygraph.py
@@ -129,13 +129,14 @@ class TestMyGraph(unittest.TestCase):
         self.assertEqual(g.data, [[1, [1,2,3]], [2, [1,3]], [3, [2,1]]], '1,3 ok')
 
     def test_has_path(self):
-        g = MyGraph([[1,[1]], [2,[3,4]], [3,[2]], [4, [2,6]], [5, []], [6, [4]]])
+        g = MyGraph([[1,[1]], [2,[3,4,7]], [3,[2]], [4, [2,6,8]], [5, []], [6, [4]], [7,[2,8]], [8,[4,7]]])
         self.assertEqual(g.has_path(2,3), 1, '2,3 ok')
         self.assertEqual(g.has_path(1,1), 1, '1,1 ok')
         #self.assertEqual(g.has_path(2,2), 0, '2,2 not there')
-# TODO: does NOT work for cycles yet
+# TODO: does NOT work for cycles of one vertex yet
         self.assertEqual(g.has_path(2,4), 1, '2,4 ok')
         self.assertEqual(g.has_path(4,2), 1, '4,2 ok')
+        self.assertEqual(g.has_path(2,8), 1, '2,8 ok')
         self.assertEqual(g.has_path(4,5), 0, '4,5 not there')
         self.assertEqual(g.has_path(2,6), 1, '2,6 ok')
         self.assertEqual(g.has_path(6,2), 1, '6,2 ok')

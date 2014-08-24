a923ccb9cfd339e3d9246220274afa41b22d87a7
Strelkova Natalia <flore@ya.ru>
2014-08-24 13:12:13 +0400

    9.13. - use set

--- 9.13.py
+++ 9.13.py
@@ -2,36 +2,21 @@
 
 from mybintree_w_parent_9_12 import MyBinTreeWithParent
 
-def lca_fast(t, n1, n2):
+def lca_fast(n1, n2):
     if n1 == n2:
         return n1
-    seen = {n1: 1, n2: 1}
+    seen = set([n1, n2])
     while n1.parent is not None or n2.parent is not None:
         if n1.parent is not None:
             n1 = n1.parent
-            ok = 0
-            try:
-                ok = seen[n1]
-            except KeyError:
-                pass
-            if ok:
+            if n1 in seen:
                 return n1
-            seen[n1] = 1
+            seen.add(n1)
         if n2.parent is not None:
             n2 = n2.parent
-            ok = 0
-            try:
-                ok = seen[n2]
-            except KeyError:
-                pass
-            if ok:
+            if n2 in seen:
                 return n2
-            seen[n2] = 1
-    for s in seen.keys():
-        if s is not None:
-            print s.value
-        else:
-            print None
+            seen.add(n2)
     return None
 
 
@@ -49,15 +34,15 @@ class TestLCAFast(unittest.TestCase):
         self.t.right.put_right(8)
 
     def test_one(self):
-        self.assertEqual(lca_fast(self.t,self.t,self.t), self.t, 'just one node')
-        self.assertEqual(lca_fast(self.t,self.t.left,self.t.left), self.t.left, 'same nodes')
+        self.assertEqual(lca_fast(self.t,self.t), self.t, 'just one node')
+        self.assertEqual(lca_fast(self.t.left,self.t.left), self.t.left, 'same nodes')
 
     def test_many(self):
-        self.assertEqual(lca_fast(self.t,self.t.left,self.t.right), self.t, 'left and right')
-        self.assertEqual(lca_fast(self.t,self.t.left.left,self.t.right), self.t, 'left.left, right')
-        self.assertEqual(lca_fast(self.t,self.t.left.right,self.t.left), self.t.left, 'left.right, left')
-        self.assertEqual(lca_fast(self.t,self.t.left.left.left,self.t.left.right), self.t.left, 'left.left.left, left.right')
-        self.assertEqual(lca_fast(self.t,self.t.left.left.left,self.t.right), self.t, 'left.left.left, right')
+        self.assertEqual(lca_fast(self.t.left,self.t.right), self.t, 'left and right')
+        self.assertEqual(lca_fast(self.t.left.left,self.t.right), self.t, 'left.left, right')
+        self.assertEqual(lca_fast(self.t.left.right,self.t.left), self.t.left, 'left.right, left')
+        self.assertEqual(lca_fast(self.t.left.left.left,self.t.left.right), self.t.left, 'left.left.left, left.right')
+        self.assertEqual(lca_fast(self.t.left.left.left,self.t.right), self.t, 'left.left.left, right')
 
 
 if __name__ == '__main__':
